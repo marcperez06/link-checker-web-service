bcc3ac139886d89ef6ae39725337f2c86848fba9
marcperez06 <marc.perez.rodriguez0@gmail.com>
2023-05-18 23:00:58 +0200

    Update test class name

--- src/test/java/io/github/marcperez06/link_checker_web_service/LinkCheckerWebServiceApplicationTest.java
+++ src/test/java/io/github/marcperez06/link_checker_web_service/LinkCheckerWebServiceApplicationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GettingStartedSpringBootApplicationTests {
+class LinkCheckerWebServiceApplicationTest {
 
 	@Test
 	void contextLoads() {
