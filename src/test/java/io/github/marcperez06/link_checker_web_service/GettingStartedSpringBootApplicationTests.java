d715d63282b9b449e8b65550a9a7de0b36b5e279
marcperez06 <marc.perez.rodriguez0@gmail.com>
2023-05-18 22:59:51 +0200

    Update package for test

--- src/test/java/io/github/marcperez06/link_checker_web_service/GettingStartedSpringBootApplicationTests.java
+++ src/test/java/io/github/marcperez06/link_checker_web_service/GettingStartedSpringBootApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.gettingstartedspringboot;
+package io.github.marcperez06.link_checker_web_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
